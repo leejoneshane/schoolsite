7991ad0bd5ccb8da0ff3973ac366e4d04e277483
leejoneshne <leejoneshane@gmail.com>
2022-08-24 17:35:50 +0800

    fix bug

--- html/resources/views/app/calendar.blade.php
+++ html/resources/views/app/calendar.blade.php
@@ -26,6 +26,14 @@
             <i class="fa-solid fa-calendar-plus"></i>新增事件
         </a>
         @endif
+        @admin
+        <a class="text-sm py-2 px-6 rounded text-blue-300 hover:text-blue-600" href="{{ route('calendar.seme') }}">
+            <i class="fa-solid fa-calendar"></i>學期行事曆
+        </a>
+        <a class="text-sm py-2 px-6 rounded text-blue-300 hover:text-blue-600" href="{{ route('calendar.traning') }}">
+            <i class="fa-solid fa-calendar-days"></i>週三行事曆
+        </a>
+        @endadmin
         @teacher
         <a class="text-sm py-2 px-6 rounded text-blue-300 hover:text-blue-600" href="{{ route('calendar.seme') }}">
             <i class="fa-solid fa-calendar"></i>學期行事曆
