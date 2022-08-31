133e8f01007f09c8e56de9a46d24294f9f76ac65
leejoneshane <leejoneshane@gmail.com>
2022-08-31 12:34:26 +0800

    User::where

--- html/app/Console/Kernel.php
+++ html/app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected function scheduleTimezone()
+    {
+        return 'Asia/Taipei';
+    }
+
     /**
      * Define the application's command schedule.
      *
