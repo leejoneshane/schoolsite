1168f25d6454df4904cd8ee73978bcb797e14bb4
leejoneshane <leejoneshane@gmail.com>
2023-06-07 15:00:02 +0800

    patch bugs

---
+++ html/app/Http/Controllers/Api/v1p1/OneRosterController.php
@@ -0,0 +1,319 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1p1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class OneRosterController extends Controller
+{
+
+    //3.1a Rostering
+    public function getAllAcademicSessions()
+    {
+
+    }
+
+    public function getAcademicSessions()
+    {
+
+    }
+
+    public function getAllClasses()
+    {
+
+    }
+
+    public function getClass()
+    {
+
+    }
+
+    public function getAllCourses()
+    {
+
+    }
+
+    public function getCourse()
+    {
+
+    }
+
+    public function getAllGradeingPeriods()
+    {
+
+    }
+
+    public function getGradeingPeriod()
+    {
+
+    }
+
+    public function getAllDemographics()
+    {
+
+    }
+
+    public function getDemographics()
+    {
+
+    }
+
+    public function getAllEnrollments()
+    {
+
+    }
+
+    public function getEnrollment()
+    {
+
+    }
+
+    public function getAllOrgs()
+    {
+
+    }
+
+    public function getOrg()
+    {
+
+    }
+
+    public function getAllSchools()
+    {
+
+    }
+
+    public function getSchool()
+    {
+
+    }
+
+    public function getAllStudents()
+    {
+
+    }
+
+    public function getStudent()
+    {
+
+    }
+
+    public function getAllTeachers()
+    {
+
+    }
+
+    public function getTeacher()
+    {
+
+    }
+
+    public function getAllTerms()
+    {
+
+    }
+
+    public function getTerm()
+    {
+
+    }
+
+    public function getAllUsers()
+    {
+
+    }
+
+    public function getUser()
+    {
+
+    }
+
+    public function getCoursesForSchool()
+    {
+
+    }
+
+    public function getEnrollmentsForClassInSchool()
+    {
+
+    }
+
+    public function getStudentsForClassInSchool()
+    {
+
+    }
+
+    public function getTeachersForClassInSchool()
+    {
+
+    }
+
+    public function getEnrollmentsForSchool()
+    {
+
+    }
+
+    public function getStudentsForSchool()
+    {
+
+    }
+
+    public function getTeachersForSchool()
+    {
+
+    }
+
+    public function getTermsForSchool()
+    {
+
+    }
+
+    public function getClassesForTerm()
+    {
+
+    }
+
+    public function getGradingPeriodsForTerm()
+    {
+
+    }
+
+    public function getClassesForCourse()
+    {
+
+    }
+
+    public function getClassesForStudent()
+    {
+
+    }
+
+    public function getClassesForTeacher()
+    {
+
+    }
+
+    public function getClassesForSchool()
+    {
+
+    }
+
+    public function getClassesForUser()
+    {
+
+    }
+
+    public function getStudentsForClass()
+    {
+
+    }
+
+    public function getTeachersForClass()
+    {
+
+    }
+
+    //3.1b Resources
+    public function getAllResources()
+    {
+
+    }
+
+    public function getResource()
+    {
+
+    }
+
+    public function getResourcesForCourse()
+    {
+
+    }
+
+    public function getResourcesForClass()
+    {
+
+    }
+
+    //3.1c Gradebooks
+    public function getAllCategories()
+    {
+
+    }
+
+    public function getCategory()
+    {
+
+    }
+
+    public function deleteCategory()
+    {
+
+    }
+
+    public function putCategory()
+    {
+
+    }
+
+    public function getAllLineItems()
+    {
+
+    }
+
+    public function getLineItem()
+    {
+
+    }
+
+    public function deleteLineItem()
+    {
+
+    }
+
+    public function putLineItem()
+    {
+
+    }
+
+    public function getAllResults()
+    {
+
+    }
+
+    public function getResult()
+    {
+
+    }
+
+    public function deleteResult()
+    {
+
+    }
+
+    public function putResult()
+    {
+
+    }
+
+    public function getResultsForClass()
+    {
+
+    }
+
+    public function getListItemsForClass()
+    {
+
+    }
+
+    public function getResultsForLineItemForClass()
+    {
+
+    }
+
+    public function getResultsForStudentForClass()
+    {
+
+    }
+}
