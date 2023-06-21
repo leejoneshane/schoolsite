5fed0305af48d102d37eedf4d6187f7281e10419
leejoneshane <leejoneshane@gmail.com>
2023-06-21 14:26:24 +0800

    修改說明文字

--- html/resources/views/app/lunch_survey.blade.php
+++ html/resources/views/app/lunch_survey.blade.php
@@ -135,60 +135,64 @@
     </div>
     @endif
 @else
-<div class="flex flex-col justify-center">
-    <div class="w-full border-blue-500 bg-blue-100 dark:bg-blue-700 border-b-2 mb-5" role="alert">
-        <p>已調查班級數：{{ $count->classes }}　累計調查人數：{{ $count->students }}</p>
-    </div>
+    <div class="flex flex-col justify-center">
+        <div class="w-full border-blue-500 bg-blue-100 dark:bg-blue-700 border-b-2 mb-5" role="alert">
+            <p>已調查班級數：{{ $count->classes }}　累計調查人數：{{ $count->students }}</p>
+        </div>
     @if ($manager)
-    <div>
-        <label for="classes" class="inline">請選擇班級：</label>
-        <select id="classes" class="inline rounded w-32 py-2 mr-6 border border-gray-300 focus:border-blue-700 focus:ring-1 focus:ring-blue-700 focus:outline-none active:outline-none dark:border-gray-400 dark:focus:border-blue-600 dark:focus:ring-blue-600  bg-white dark:bg-gray-700 text-black dark:text-gray-200"
+        <div>
+            <label for="classes" class="inline">請選擇班級：</label>
+            <select id="classes" class="inline rounded w-32 py-2 mr-6 border border-gray-300 focus:border-blue-700 focus:ring-1 focus:ring-blue-700 focus:outline-none active:outline-none dark:border-gray-400 dark:focus:border-blue-600 dark:focus:ring-blue-600  bg-white dark:bg-gray-700 text-black dark:text-gray-200"
             onchange="
             var cls = this.value;
             window.location.replace('{{ route('lunch', ['section' => $section]) }}' + '?class=' + cls);
-        ">
-            <option></option>
-            @foreach ($classes as $cls)
-            <option value="{{ $cls->id }}"{{ ($classroom->id == $cls->id) ? ' selected' : '' }}>{{ $cls->name }}</option>
-            @endforeach
-        </select>    
-    </div>
+            ">
+                <option></option>
+                @foreach ($classes as $cls)
+                <option value="{{ $cls->id }}"{{ ($classroom->id == $cls->id) ? ' selected' : '' }}>{{ $cls->name }}</option>
+                @endforeach
+            </select>
+        </div>
+    @elseif ($classroom)
+        <div>
+            班級：{{ $classroom->name }}
+        </div>
     @else
-    <div>
-        班級：{{ $classroom->name }}
-    </div>
+        <div class="font-bold">非級任導師無法查看午餐調查結果！</div>
     @endif
-    <table class="border border-1 p-3 w-3/4">
-        <tr>
-            <th scope="col" rowspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">班級</th>
-            <th scope="col" rowspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">座號</th>
-            <th scope="col" rowspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">姓名</th>
-            <th scope="col" colspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">參加午餐</th>
-            <th scope="col" colspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">乳品</th>
-            <th scope="col" colspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">不參加午餐</th>
-        </tr>
-        <tr>
-            <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">葷食</th>
-            <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">素食</th>
-            <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">要飲用</th>
-            <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">改成豆乳</th>
-            <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">家長親送</th>
-            <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">蒸飯設備</th>
-        </tr>
-        @foreach ($surveys as $s)
-        <tr>
-            <td class="border border-1 text-sm text-center">{{ $s->student->classroom->name }}</td>
-            <td class="border border-1 text-sm text-center">{{ $s->student->seat }}</td>
-            <td class="border border-1 text-sm text-center">{{ $s->student->realname }}</td>
-            <td class="border border-1 text-sm text-center">{!! ($s->by_school && !($s->vegen)) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
-            <td class="border border-1 text-sm text-center">{!! ($s->by_school && $s->vegen) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
-            <td class="border border-1 text-sm text-center">{!! ($s->by_school && $s->milk) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
-            <td class="border border-1 text-sm text-center">{!! ($s->by_school && !($s->milk)) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
-            <td class="border border-1 text-sm text-center">{!! ($s->by_parent) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
-            <td class="border border-1 text-sm text-center">{!! ($s->boxed_meal) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
-        </tr>
-        @endforeach
-    </table>
+    @if ($classroom)
+        <table class="border border-1 p-3 w-3/4">
+            <tr>
+                <th scope="col" rowspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">班級</th>
+                <th scope="col" rowspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">座號</th>
+                <th scope="col" rowspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">姓名</th>
+                <th scope="col" colspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">參加午餐</th>
+                <th scope="col" colspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">乳品</th>
+                <th scope="col" colspan="2" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">不參加午餐</th>
+            </tr>
+            <tr>
+                <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">葷食</th>
+                <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">素食</th>
+                <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">要飲用</th>
+                <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">改成豆乳</th>
+                <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">家長親送</th>
+                <th scope="col" class="border border-1 text-sm font-bold bg-gray-100 dark:bg-gray-500">蒸飯設備</th>
+            </tr>
+            @foreach ($surveys as $s)
+            <tr>
+                <td class="border border-1 text-sm text-center">{{ $s->student->classroom->name }}</td>
+                <td class="border border-1 text-sm text-center">{{ $s->student->seat }}</td>
+                <td class="border border-1 text-sm text-center">{{ $s->student->realname }}</td>
+                <td class="border border-1 text-sm text-center">{!! ($s->by_school && !($s->vegen)) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
+                <td class="border border-1 text-sm text-center">{!! ($s->by_school && $s->vegen) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
+                <td class="border border-1 text-sm text-center">{!! ($s->by_school && $s->milk) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
+                <td class="border border-1 text-sm text-center">{!! ($s->by_school && !($s->milk)) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
+                <td class="border border-1 text-sm text-center">{!! ($s->by_parent) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
+                <td class="border border-1 text-sm text-center">{!! ($s->boxed_meal) ? '<i class="fa-solid fa-check"></i>' : ''  !!}</td>
+            </tr>
+            @endforeach
+        </table>
+    @endif
 </div>
 @endif
 </div>
