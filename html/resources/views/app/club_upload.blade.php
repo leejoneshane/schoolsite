f7b4c2a32b4d29630859f9a71f79a4f869b1d04a
leejoneshane <leejoneshane@gmail.com>
2024-04-03 11:00:54 +0800

    patch bugs

--- html/resources/views/app/club_upload.blade.php
+++ html/resources/views/app/club_upload.blade.php
@@ -95,7 +95,7 @@
     </div>
     <div class="p-3">
         <span class="sr-only">請選擇上傳檔案！</span>
-        <input type="file" name="excel" accept=".xls,.xlsx,.pdf" class="block text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" required>
+        <input type="file" name="excel" accept=".xlsx" class="block text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" required>
     </div>
     <div class="p-3">
         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-6 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
