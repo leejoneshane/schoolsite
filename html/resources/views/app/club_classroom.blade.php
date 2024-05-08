c5fb88848b86670e9fcc662c1407d5488e439284
leejoneshane <leejoneshane@gmail.com>
2024-05-08 12:41:45 +0800

    修改版面

--- html/resources/views/app/club_classroom.blade.php
+++ html/resources/views/app/club_classroom.blade.php
@@ -31,21 +31,21 @@
     <option value="{{ $class->id }}"{{ ($class->id == $class_id) ? ' selected' : '' }}>{{ $class->name }}</option>
     @endforeach
 </select>
-<table class="w-full py-4 text-left font-normal">
-    <tr class="bg-gray-300 dark:bg-gray-500 font-semibold text-lg">
-        <th scope="col" class="p-2">
+<table class="border w-full py-4 text-left font-normal">
+    <tr class="border bg-gray-300 dark:bg-gray-500 font-semibold text-lg">
+        <th scope="col" class="border p-2">
             座號
         </th>
-        <th scope="col" class="p-2">
+        <th scope="col" class="border p-2">
             姓名
         </th>
-        <th scope="col" class="p-2">
+        <th scope="col" class="border p-2">
             社團全名
         </th>
-        <th scope="col" class="p-2">
+        <th scope="col" class="border p-2">
             上課時間
         </th>
-        <th scope="col" class="p-2">
+        <th scope="col" class="border p-2">
             授課地點
         </th>
     </tr>
@@ -54,19 +54,19 @@
             $count = $students->count();
         @endphp
         @foreach ($students as $enroll)
-    <tr class="odd:bg-white even:bg-gray-100 dark:odd:bg-gray-700 dark:even:bg-gray-600">
+    <tr class="border dark:odd:bg-gray-700 dark:even:bg-gray-600">
         @if ($loop->first)
-        <td rowspan="{{ $count }}" class="p-2">{{ $enroll->student->seat }}</td>
-        <td rowspan="{{ $count }}" class="p-2">{{ $enroll->student->realname }}</td>
+        <td rowspan="{{ $count }}" class="border p-2">{{ $enroll->student->seat }}</td>
+        <td rowspan="{{ $count }}" class="border p-2">{{ $enroll->student->realname }}</td>
         @endif
-        <td class="p-2 {{ $enroll->club->style }}">{{ $enroll->club->name }}</td>
-        <td class="p-2">{{ $enroll->studytime }}</td>
-        <td class="p-2">{{ $enroll->club->location }}</td>
+        <td class="border p-2 {{ $enroll->club->style }}">{{ $enroll->club->name }}</td>
+        <td class="border p-2">{{ $enroll->studytime }}</td>
+        <td class="border p-2">{{ $enroll->club->location }}</td>
     </tr>
         @endforeach
     @empty
     <tr>
-        <td colspan="8" class="text-xl font-bold">目前查無已錄取的學生！</td>
+        <td colspan="8" class="border text-xl font-bold">目前查無已錄取的學生！</td>
     </tr>
     @endforelse
 </table>
