6770db1672b278d19646726893bfa5817b6f95d6
leejoneshane <leejoneshane@gmail.com>
2024-09-23 16:00:37 +0800

    fix bug

--- html/app/Http/Controllers/Admin/SchoolDataController.php
+++ html/app/Http/Controllers/Admin/SchoolDataController.php
@@ -361,6 +361,8 @@ class SchoolDataController extends Controller
                 DB::table('assignment')->where('id', $old->id)->delete();
             } 
         }
+        $new_classes = array_values($new_classes);
+        $new_subjects = array_values($new_subjects);
         if (!empty($new_classes)) {
             for ($i=0; $i<count($new_classes); $i++) {
                 DB::table('assignment')->insertOrIgnore([
