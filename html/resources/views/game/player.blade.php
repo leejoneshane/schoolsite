eb4e9d84bd9f8798b9d52c79ae1b57915c2d4b47
leejoneshane <leejoneshane@gmail.com>
2024-09-25 21:18:48 +0800

    調整版面

--- html/resources/views/game/player.blade.php
+++ html/resources/views/game/player.blade.php
@@ -12,13 +12,13 @@
                 <td colspan="2">{{ $character->seat }} {{ $character->name }}</td>
             </tr>
             <tr>
-                <td>職業</td><td>{{ $character->profession->name }}</td>
+                <td class="w-16">職業</td><td>{{ $character->profession->name }}</td>
             </tr>
             <tr>
-                <td>等級</td><td>{{ $character->level }}</td>
+                <td class="w-16">等級</td><td>{{ $character->level }}</td>
             </tr>
             <tr>
-                <td>HP</td>
+                <td class="w-16">HP</td>
                 <td>
                     <div class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700 text-right leading-none text-xs font-medium">
                         <div id="hp" class="h-4 bg-green-600 text-xs font-medium text-green-100 text-center p-0.5 leading-none rounded-full" style="width: {{ intval($character->hp / $character->max_hp * 100) }}%">{{ $character->hp }}</div>
@@ -26,7 +26,7 @@
                 </td>
             </tr>
             <tr>
-                <td>MP</td>
+                <td class="w-16">MP</td>
                 <td>
                     <div class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700 text-right leading-none text-xs font-medium">
                         <div id="hp" class="h-4 bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: {{ intval($character->mp / $character->max_mp * 100) }}%">{{ $character->mp }}</div>
@@ -34,57 +34,71 @@
                 </td>
             </tr>
             <tr>
-                <td>AP</td><td><span class="text-red-500">{{ $character->final_ap }}</span>[<span class="text-blue-500">{{ $character->ap }}</span>]</td>
+                <td class="w-16">AP</td><td><span class="text-red-500">{{ $character->final_ap }}</span>[<span class="text-blue-500">{{ $character->ap }}</span>]</td>
             </tr>
             <tr>
-                <td>DP</td><td><span class="text-red-500">{{ $character->final_dp }}</span>[<span class="text-blue-500">{{ $character->dp }}</span>]</td>
+                <td class="w-16">DP</td><td><span class="text-red-500">{{ $character->final_dp }}</span>[<span class="text-blue-500">{{ $character->dp }}</span>]</td>
             </tr>
             <tr>
-                <td>SP</td><td><span class="text-red-500">{{ $character->final_sp }}</span>[<span class="text-blue-500">{{ $character->sp }}</span>]</td>
+                <td class="w-16">SP</td><td><span class="text-red-500">{{ $character->final_sp }}</span>[<span class="text-blue-500">{{ $character->sp }}</span>]</td>
             </tr>
             <tr>
-                <td>狀態</td><td><span class="text-red-500">{{ $character->status_desc }}</span>[<span class="text-blue-500">{{ $character->status_desc }}</span>]</td>
+                <td class="w-16">狀態</td><td><span class="text-blue-500">{{ $character->status_desc }}</span></td>
             </tr>
             <tr>
-                <td>XP</td><td><span class="text-lime-500">{{ $character->xp }}</span></td>
+                <td class="w-16">XP</td><td><span class="text-lime-500">{{ $character->xp }}</span></td>
             </tr>
             <tr>
-                <td>GP</td><td><span class="text-lime-500">{{ $character->gp }}</span></td>
+                <td class="w-16">GP</td><td><span class="text-lime-500">{{ $character->gp }}</span></td>
             </tr>
         </table>
-        <table class="w-full text-left font-normal pb-6">
-            <tr>
-                <td colspan="5" class="bg-gray-500 text-white">背包</td>
-            </tr>
-            <tr>
-                <td colspan="5" class="h-3"></td>
-            </tr>
-            @for ($i=0; $i<5; $i++)
-            <tr class="h-12">
-                <td class="w-2"></td>
-                @for ($j=1; $j<4; $j++)
-                <td id="bag{{ $j + $i * 3 }}" class="w-12"></td>
+        <table class="w-full">
+            <thead>
+                <tr>
+                    <td colspan="5" class="bg-gray-500 text-white">背包</td>
+                </tr>    
+            </thead>
+            <tbody class="bg-game-bag bg-contain bg-no-repeat">
+                <tr>
+                    <td colspan="5" class="h-8"></td>
+                </tr>
+                @for ($i=0; $i<4; $i++)
+                <tr class="h-24">
+                    <td class="w-10"></td>
+                    @for ($j=1; $j<4; $j++)
+                    <td id="bag{{ $j + $i * 3 }}" class="w-20"></td>
+                    @endfor
+                    <td class="w-10"></td>
+                </tr>
                 @endfor
-                <td class="w-2"></td>
-            </tr>
-            @endfor
-            <tr>
-                <td colspan="5" class="h-3"></td>
-            </tr>
+                <tr>
+                    <td colspan="5" class="h-10"></td>
+                </tr>
+            </tbody>
         </table>
     </div>
     <div class="w-1/2 relative">
     </div>
     <div class="w-1/4 bg-white">
-        <table class="w-full text-left font-normal">
+        <table class="w-full h-[50rem]text-left font-normal">
+        <thead>
             <tr>
                 <td colspan="3" class="bg-gray-500 text-white">技能書</td>
             </tr>
+        </thead>
+        <tbody class=" bg-game-book bg-cover bg-opacity-50">
+            <tr>
+                <td colspan="3" class="h-8"></td>
+            </tr>
             @foreach ($character->profession->skills as $skill)
-            <tr class="text-lg p-4">
+            <tr class="text-lg p-8">
                 <td class="w-8">{{ $skill->pivot->level }}</td><td class="w-32">{{ $skill->name }}</td><td class="w-auto text-xs">{{ $skill->description }}</td>
             </tr>
             @endforeach
+            <tr>
+                <td colspan="3" class="h-64"></td>
+            </tr>
+        </tbody>
         </table>
     </div>
 </div>
